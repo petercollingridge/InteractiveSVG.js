f0b6ee9797a8f0d75de47f144273950f5fe6d8f4
Peter Collingridge <peter.collingridge@gmail.com>
2018-01-10 15:12:30 +0000

    Try new approach to generating SVG

--- interactiveSVG.js
+++ interactiveSVG.js
@@ -1,3 +1,16 @@
+var LineSegmentFromPoints = function(canvas, p1, p2) {
+    this.canvas = canvas;
+    this.p1 = p1;
+    this.p2 = p2;
+
+    var defaultAttr = { class: 'line-segment' }
+    defaultAttr.x1 = p1.attr('cx');
+    defaultAttr.y1 = p1.attr('cy');
+    defaultAttr.x2 = p2.attr('cx');
+    defaultAttr.y2 = p2.attr('cy');
+    this.el = canvas.addElement('line', defaultAttr);
+}
+
 var InteractiveSVGCanvas = function(id) {
     this.svgElement = $('#' + id);
     this.selected = false;
@@ -9,7 +22,10 @@ InteractiveSVGCanvas.prototype._addMouseEventHandlers = function() {
 
     this.svgElement.on('mousemove', function(evt) {
         if (self.selected) {
+            // Get dragging to work on touch device
             if (evt.type === 'touchmove') { evt = evt.touches[0]; }
+
+            // Move based on change in mouse position
             var x = parseFloat(self.selected.attr('cx')) + evt.clientX - self.dragX;
             var y = parseFloat(self.selected.attr('cy')) + evt.clientY - self.dragY;
 
@@ -50,4 +66,37 @@ InteractiveSVGCanvas.prototype.addDraggablePoint = function(attr) {
     $.extend(defaultAttr, attr);
     var $el = this.addElement('circle', defaultAttr);
     $el.on('mousedown', this._startDrag.bind(this));
+    return $el;
+};
+
+InteractiveSVGCanvas.prototype.addLineFromPoints = function(p1, p2, attr) {
+    return LineSegmentFromPoints(this, p1, p2);
+};
+
+
+var InteractiveSVG = function($container, width, height) {
+    this.$svg = $(document.createElementNS('http://www.w3.org/2000/svg', 'svg'));
+    this.$svg.attr({
+        xmlns: "http://www.w3.org/2000/svg",
+        width: width || 400,
+        height: height || 400
+    }).appendTo($container);
+
+    this.selected = false;
+    //this._addMouseEventHandlers();
+};
+
+InteractiveSVG.createFromJSON = function(data) {
+    if (!data.id) {
+        console.error("No id given");
+        return;
+    }
+
+    var $container = $('#' + data.id);
+    if (!$container) {
+        console.error("No element found with id " + id);
+        return;   
+    }
+
+    return new InteractiveSVG($container, data.width, data.height);
 };
