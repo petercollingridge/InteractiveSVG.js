b8d7779fd59d98999c327a4a3e179f3b64e45b59
Peter Collingridge <peter.collingridge@gmail.com>
2018-01-10 21:41:27 +0000

    Add code for creating lines from points

--- interactiveSVG.js
+++ interactiveSVG.js
@@ -1,89 +1,84 @@
-var LineSegmentFromPoints = function(canvas, p1, p2) {
-    this.canvas = canvas;
+var xmlns = 'http://www.w3.org/2000/svg';
+
+/*************************************************
+ *      LineSegmentFromPoints
+ *  A line between two draggable points
+**************************************************/
+
+var LineSegmentFromPoints = function(svgObject, p1, p2, attr) {
+    this.$svg = svgObject.$svg;
     this.p1 = p1;
     this.p2 = p2;
 
-    var defaultAttr = { class: 'line-segment' }
-    defaultAttr.x1 = p1.attr('cx');
-    defaultAttr.y1 = p1.attr('cy');
-    defaultAttr.x2 = p2.attr('cx');
-    defaultAttr.y2 = p2.attr('cy');
-    this.el = canvas.addElement('line', defaultAttr);
+    var defaultAttr = {
+        class: 'line-segment',
+        x1: p1.x,
+        y1: p1.y,
+        x2: p2.x,
+        y2: p2.y
+    };
+
+    this.$element = svgObject.addElement('line', defaultAttr);
 }
 
-var InteractiveSVGCanvas = function(id) {
-    this.svgElement = $('#' + id);
-    this.selected = false;
-    this._addMouseEventHandlers();
-};
+/*************************************************
+ *      DraggablePoint
+ *  A point that can be dragged.
+**************************************************/
 
-InteractiveSVGCanvas.prototype._addMouseEventHandlers = function() {
-    var self = this;
+var DraggablePoint = function(svgObject, x, y, attr) {
+    this.$svg = svgObject.$svg;
+    this.x = x || 0;
+    this.y = y || 0;
 
-    this.svgElement.on('mousemove', function(evt) {
-        if (self.selected) {
-            // Get dragging to work on touch device
-            if (evt.type === 'touchmove') { evt = evt.touches[0]; }
-
-            // Move based on change in mouse position
-            var x = parseFloat(self.selected.attr('cx')) + evt.clientX - self.dragX;
-            var y = parseFloat(self.selected.attr('cy')) + evt.clientY - self.dragY;
-
-            self.selected.attr('cx', x);
-            self.selected.attr('cy', y);
-            self.dragX = evt.clientX;
-            self.dragY = evt.clientY;
-        }
-    });
-
-    this.svgElement.on('mouseup', function() {
-        self.selected = false;
-    });
-};
-
-InteractiveSVGCanvas.prototype._startDrag = function(evt) {
-    this.selected = $(evt.target);
-
-    // Get dragging to work on touch device
-    if (evt.type === 'touchstart') { evt = evt.touches[0]; }
-    this.dragX = evt.clientX;
-    this.dragY = evt.clientY;
-};
-
-InteractiveSVGCanvas.create = function(id) {
-    return new InteractiveSVGCanvas(id);
-};
-
-InteractiveSVGCanvas.prototype.addElement = function(tagName, attr) {
-    var $el = $(document.createElementNS('http://www.w3.org/2000/svg', tagName));
-    $el.attr(attr);
-    this.svgElement.append($el);
-    return $el;
-};
-
-InteractiveSVGCanvas.prototype.addDraggablePoint = function(attr) {
-    var defaultAttr = { r: 6, class: 'draggable-point' }
+    var defaultAttr = { cx: x, cy: y, r: 6, class: 'draggable-point' }
     $.extend(defaultAttr, attr);
-    var $el = this.addElement('circle', defaultAttr);
-    $el.on('mousedown', this._startDrag.bind(this));
-    return $el;
+    this.$element = svgObject.addElement('circle', defaultAttr);
+
+    // Add handler to allow dragging
+    var self = this;
+    this.$element.on('mousedown', function(evt) {
+        svgObject.selected = self;
+
+        // Get dragging to work on touch device
+        if (evt.type === 'touchstart') { evt = evt.touches[0]; }
+        svgObject.dragX = evt.clientX;
+        svgObject.dragY = evt.clientY;
+    });
 };
 
-InteractiveSVGCanvas.prototype.addLineFromPoints = function(p1, p2, attr) {
-    return LineSegmentFromPoints(this, p1, p2);
+DraggablePoint.prototype.move = function(dx, dy) {
+    this.x += dx;
+    this.y += dy;
+    this.$element.attr({ cx: this.x, cy: this.y });
 };
 
+/*************************************************
+ *      InteractiveSVG
+ *  Main object for the whole SVG.
+**************************************************/
 
 var InteractiveSVG = function($container, width, height) {
-    this.$svg = $(document.createElementNS('http://www.w3.org/2000/svg', 'svg'));
+    this.$svg = $(document.createElementNS(xmlns, 'svg'));
     this.$svg.attr({
-        xmlns: "http://www.w3.org/2000/svg",
+        xmlns: xmlns,
         width: width || 400,
         height: height || 400
     }).appendTo($container);
 
+    this.points = {};
+    this.lines = {};
     this.selected = false;
-    //this._addMouseEventHandlers();
+    this._addMouseEventHandlers();
+};
+
+InteractiveSVG.create = function(id, width, height) {
+    var $container = $('#' + id);
+    if (!$container) {
+        console.error("No element found with id " + id);
+        return;   
+    }
+    return new InteractiveSVG($container, width, height);
 };
 
 InteractiveSVG.createFromJSON = function(data) {
@@ -92,11 +87,76 @@ InteractiveSVG.createFromJSON = function(data) {
         return;
     }
 
-    var $container = $('#' + data.id);
-    if (!$container) {
-        console.error("No element found with id " + id);
-        return;   
+    var svgObject = this.create(data.id, data.width, data.height);
+
+    if (data.points) {
+        for (var label in data.points) {
+            svgObject.addPoint(label, data.points[label]);
+        }
     }
 
-    return new InteractiveSVG($container, data.width, data.height);
+    if (data.lines) {
+        for (var label in data.lines) {
+            svgObject.addLine(label, data.lines[label]);
+        }
+    }
+
+    return svgObject;
+};
+
+InteractiveSVG.prototype._addMouseEventHandlers = function() {
+    var self = this;
+
+    this.$svg.on('mousemove', function(evt) {
+        if (self.selected) {
+            // Get dragging to work on touch device
+            if (evt.type === 'touchmove') { evt = evt.touches[0]; }
+
+            // Move based on change in mouse position
+            var dx = evt.clientX - self.dragX;
+            var dy = evt.clientY - self.dragY;
+            self.selected.move(dx, dy);
+
+            // Update mouse position
+            self.dragX = evt.clientX;
+            self.dragY = evt.clientY;
+        }
+    });
+
+    this.$svg.on('mouseup', function() {
+        self.selected = false;
+    });
+};
+
+InteractiveSVG.prototype.addPoint = function(label, attr) {
+    // Extract x and y coordinates from attr hash
+    var x = attr.x;
+    var y = attr.y;
+    delete attr.x;
+    delete attr.y;
+
+    var point = new DraggablePoint(this, x, y, attr);
+    this.points[label] = point;
+    return point;
+};
+
+InteractiveSVG.prototype.addLine = function(label, attr) {
+    // Extract x and y coordinates from attr hash
+    var p1 = attr.p1;
+    var p2 = attr.p2;
+    delete attr.p1;
+    delete attr.p2;
+
+    if (typeof p1 === 'string') { p1 = this.points[p1]; }
+    if (typeof p2 === 'string') { p2 = this.points[p2]; }
+
+    var line = new LineSegmentFromPoints(this, p1, p2, attr);
+    this.lines[label] = line;
+    return line;
+};
+
+InteractiveSVG.prototype.addElement = function(tagName, attr) {
+    return $(document.createElementNS(xmlns, tagName))
+            .attr(attr)
+            .appendTo(this.$svg);
 };
